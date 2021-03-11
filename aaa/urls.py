adef369dbdf0e2667d338bc6030f513d80543d61
Junha Moon <ririro93@gmail.com>
2021-03-11 21:50:35 +0900

    Feat: create line test

--- aaa/urls.py
+++ aaa/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import index
+from .views import (
+    index,
+    line,
+)
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('line/', line, name='line'),
 ]
---
+++ static/js/line.js
@@ -0,0 +1,36 @@
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.module.min.js';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+
+scene.add( line );
+
+const animate = function () {
+  requestAnimationFrame( animate );
+
+  line.rotation.x += 0.01;
+  line.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+};
+
+animate();
