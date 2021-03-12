8f3bfc31dff181806eb7c8c3204e7dab08875c8f
Junha Moon <ririro93@gmail.com>
2021-03-12 20:40:41 +0900

    Feat: box material changes on window click

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,62 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.module.min.js';
 
 let camera, scene, renderer;
-let geometry, material, mesh;
+let geometry, material, mesh, texture;
 
 init();
+window.addEventListener("resize", onWindowResize, false); 
+window.addEventListener("click", onWindowClick, false); 
 
 function init() {
-
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
-
+	// Scene
 	scene = new THREE.Scene();
 
+	// Camera
+	camera = new THREE.PerspectiveCamera( 
+		70,
+		window.innerWidth / window.innerHeight,
+		0.01,
+		10 
+	);
+	camera.position.z = 1;
+	
+	// Box
 	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
+	const randTextureNum = Math.floor(Math.random() * Math.floor(5)) + 1;
+	console.log(randTextureNum);
+	texture = new THREE.TextureLoader().load(`static/textures/${randTextureNum}.jpg`);
+	material = new THREE.MeshBasicMaterial({ map: texture });
+	// material = new THREE.MeshNormalMaterial();
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
-
+	
+	// Renderer
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animation );
 	document.body.appendChild( renderer.domElement );
-
+	
 }
 
 function animation( time ) {
-
+	
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
-
+	
 	renderer.render( scene, camera );
+	
+}
 
+function onWindowResize() {
+	console.log('window resized');
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onWindowClick() {
+	console.log('window clicked');
+	const randTextureNum = Math.floor(Math.random() * Math.floor(4)) + 1;
+	texture = new THREE.TextureLoader().load(`static/textures/${randTextureNum}.jpg`);
+	material.map = texture;
 }
